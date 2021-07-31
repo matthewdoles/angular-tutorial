c0affbdac3838f941815dc051d9cd22ea0680a83
Matthew Doles <matteddied@gmail.com>
2021-07-31 14:05:32 -0500

    5.5 - Assignment 4: Property & Event Binding, View Encapsulation

--- components-and-databinding/src/app/app.component.ts
+++ components-and-databinding/src/app/app.component.ts
@@ -10,6 +10,9 @@ export class AppComponent {
     { type: 'server', name: 'Testserver', content: 'Just a test!' },
   ];
 
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
   onServerAdded(serverData: { serverName: string; serverContent: string }) {
     this.serverElements.push({
       type: 'server',
@@ -35,5 +38,13 @@ export class AppComponent {
 
   onDeleteFirst() {
     this.serverElements.splice(0, 1);
+  }
+
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
   }
 }
