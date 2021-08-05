0a0913dd85f5b84281a005b10a74aff430111dfd
Matthew Doles <matteddied@gmail.com>
2021-08-05 09:14:36 -0400

    8.1 - Building & Using a Dropdown Directive

---
+++ directives-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+import { Recipe } from '../recipes.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test.',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574'
+    ),
+    new Recipe(
+      'A Test Recipe 2',
+      'This is simply a test.',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+}
