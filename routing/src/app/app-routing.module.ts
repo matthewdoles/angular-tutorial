8c81279071c675aa421374d9a4b92d2e7f988554
Matthew Doles <matteddied@gmail.com>
2021-09-14 19:22:15 -0400

    11.10 - Protecting Child Routes & canDeactivate

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -19,10 +20,15 @@ const appRoutes: Routes = [
   {
     path: 'servers',
     component: ServersComponent,
-    canActivate: [AuthGuard],
+    //canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
     children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
+      {
+        path: ':id/edit',
+        component: EditServerComponent,
+        canDeactivate: [CanDeactivateGuard],
+      },
     ],
   },
   {
